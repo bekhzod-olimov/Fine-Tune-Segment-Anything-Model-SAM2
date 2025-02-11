0444014922aac1d31004f72b888fecd1189e14d2
bekhzod-olimov <bekhzod.olimov@gmail.com>
2025-02-11 13:44:50 +0900

    update checkpoint downloader script

--- checkpoints/download_ckpts.sh
+++ checkpoints/download_ckpts.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-
 # Define the URLs for the checkpoints
 BASE_URL="https://dl.fbaipublicfiles.com/segment_anything_2/072824/"
 sam2_hiera_t_url="${BASE_URL}sam2_hiera_tiny.pt"
@@ -15,19 +6,25 @@ sam2_hiera_b_plus_url="${BASE_URL}sam2_hiera_base_plus.pt"
 sam2_hiera_l_url="${BASE_URL}sam2_hiera_large.pt"
 
 # Create the checkpoints directory if it doesn't exist
-mkdir -p checkpoints
+
+root_dir="backup"
+save_dir="${root_dir}/checkpoints"
+cd ../
+mkdir -p ${root_dir}
+mkdir -p ${save_dir}
+cd fine-tune-train_segment_anything_2_in_60_lines_of_code
 
 # Download each of the four checkpoints using wget
 echo "Downloading sam2_hiera_tiny.pt checkpoint..."
-wget -P checkpoints $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
+wget -P ../${save_dir} $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
 
 echo "Downloading sam2_hiera_small.pt checkpoint..."
-wget -P checkpoints $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
+wget -P ../${save_dir} $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
 
 echo "Downloading sam2_hiera_base_plus.pt checkpoint..."
-wget -P checkpoints $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
+wget -P ../${save_dir} $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
 
 echo "Downloading sam2_hiera_large.pt checkpoint..."
-wget -P checkpoints $sam2_hiera_l_url || { echo "Failed to download checkpoint from $sam2_hiera_l_url"; exit 1; }
+wget -P ../${save_dir} $sam2_hiera_l_url || { echo "Failed to download checkpoint from $sam2_hiera_l_url"; exit 1; }
 
 echo "All checkpoints are downloaded successfully."
